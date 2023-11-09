ff900d3fd7acc0cdac9ee8b3f608afe1a119e032
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2023-11-09 23:32:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,77 @@
+from ragFunction import rag
+# from GenerateQuestion import generateQuestion
+from load_document import ingestDocument
+from chatLLM import chat
 
+!ngrok authtoken 2XfYduA6VOo2E80FKpzv2RC0H9Q_6Nppf5zANR49GdiweTeip
+
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template,request,jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+run_with_ngrok(app)
+CORS(app)
+
+@app.route('/')
+def text():
+  return f"Running Flask on Google Colab"
+
+@app.route('/ingest', methods=['POST'])
+def receive_data():
+
+  headers = {'Content-Type': 'application/json'}
+  data = request.get_json()
+  print(data)
+  print("Ingesting...")
+  #not to be used still underdevlopment
+  # can generate a request though to ingest the document 
+  ingestDocument()
+
+  response = {'message': "demo result"}
+  return jsonify(response)
+
+@app.route('/question', methods=['POST'])
+def receive_data():
+
+  headers = {'Content-Type': 'application/json'}
+  data = request.get_json()
+
+  print(data)
+  print("Generating question...")
+  print("Under Development...")
+  # generateQuestion()#not to be used still underdevlopment
+
+  response = {'message': "demo result"}
+  return jsonify(response)
+  
+@app.route('/rag', methods=['POST'])
+def receive_data():
+  headers = {'Content-Type': 'application/json'}
+  data = request.get_json()
+  print(data)
+   
+  ans, docs, time = rag(query)## ans will have the answer and docs will have the source document time is the time taken to answer
+  print("Answer is : ", ans)
+  print("Source Document is : ", docs)
+  print("Time taken is : ", time)
+
+  response = {'answer': ans, "source" : docs, "time" : time}
+  return jsonify(response)
+
+@app.route('/chat', methods=['POST'])
+def receive_data():
+
+  headers = {'Content-Type': 'application/json'}
+  data = request.get_json()
+  print(data)
+   
+  ans, time = chat(query)## ans will have the answer and time is the time taken to answer
+  print("Answer is : ", ans)
+  print("Time taken is : ", time)
+
+  response = {'answer': ans, "time" : time}
+  return jsonify(response)
+
+app.run()
